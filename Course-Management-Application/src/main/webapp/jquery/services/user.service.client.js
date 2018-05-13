0c3f6a3e57c46d0fd575e14721560ae741c03892
Vaibhav Rangarajan <rangarajan.v@husky.neu.edu>
2018-05-13 03:14:41 -0400

    Added service for admin user

---
+++ Course-Management-Application/src/main/webapp/jquery/services/user.service.client.js
@@ -0,0 +1,31 @@
+function UserServiceClient() {
+    this.createUser = createUser;
+    this.findAllUsers = findAllUsers;
+//    this.deleteUser = deleteUser;
+//    this.findUserById = findUserById;
+//    this.updateUser = updateUser;
+//    this.login = login();
+    this.url =
+        'http://localhost:8080/api/user';
+    this.login =
+        'http://localhost:8080/api/login';
+    var self = this;
+    
+    
+    function findAllUsers() {
+        return fetch(self.url)
+            .then(function (response) {
+                return response.json();
+            });
+    }
+    
+    function createUser(user) {
+        return fetch(self.url, {
+            method: 'post',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
+    }
+}
