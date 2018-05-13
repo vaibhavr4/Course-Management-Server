0c6fe6d5974aad3c55e4c5eadc7a0b37e91aa9cb
Vaibhav Rangarajan <rangarajan.v@husky.neu.edu>
2018-05-13 01:15:57 -0400

    Added service,model and repository for admin page to add user

---
+++ Course-Management-Application/src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
@@ -0,0 +1,68 @@
+//IIFE
+(function () {
+
+    jQuery(main);
+
+    var tbody;
+    var template;
+
+    function main() {
+        tbody = $('tbody');
+        template = $('.template');
+        $('#createUser').click(createUser);
+
+        var promise = fetch('http://localhost:8080/api/user');
+        promise.then(function (response) {
+            return response.json();
+        }).then(renderUsers)
+    }
+
+    function createUser() {
+        console.log('createUser');
+
+        var username = $('#usernameFld').val();
+        var password = $('#passwordFld').val();
+        var firstName = $('#firstNameFld').val();
+        var lastName = $('#lastNameFld').val();
+
+        var user = {
+            username: username,
+            password: password,
+            firstName: firstName,
+            lastName: lastName
+        };
+
+        fetch('http://localhost:8080/api/user', {
+            method: 'post',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
+    }
+
+    function renderUsers(users) {
+        for(var i=0; i<users.length; i++) {
+            var user = users[i];
+            var clone = template.clone();
+            clone.find('.username')
+                .html(user.username);
+            clone.find('.password')
+            .html(user.password);
+            clone.find('.firstName')
+            .html(user.firstName);
+            clone.find('.lastName')
+            .html(user.lastName);
+            tbody.append(clone);
+        }
+    }
+    
+    $('table td').each(function(){
+        var $this = $(this);
+        if ($this.find('.password')) 
+        	{
+        	$this.hide();
+        	}
+    })
+})();
+
